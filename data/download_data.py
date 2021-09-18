d36538fd96ec5ac5a00dd42d30533b3247f9a9f0
Simon Steshin <steshinsimon@gmail.com>
2021-09-18 18:28:03 +0300

    Add downloading offical/explore dataset
    Former-commit-id: d3ef19ded864480f49136a5b385e33796c3fe130

--- data/download_data.py
+++ data/download_data.py
@@ -6,8 +6,13 @@ def download_official():
 
     # Dict from s3 source path to target location
     source_to_target = {
+        # Predict Modality dataset
         's3://openproblems-bio/public/phase1-data/predict_modality/':
             'data/official/predict_modality/',
+        
+        # Explore dataset
+        's3://openproblems-bio/public/explore/':
+            'data/official/explore/',
     }
 
     for source_path, target_path in source_to_target.items():
