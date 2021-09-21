a7b74d27210e3c088b6f52e5e1d2ae746ecfbd99
Simon Steshin <steshinsimon@gmail.com>
2021-09-21 15:56:45 +0300

    Delete official baseline
    Former-commit-id: fa6f68dc3c1631dd15a0aeb48cf40680bd8fb474
    Former-commit-id: 075dc795aec6b66a5b65adca78e859d394c6985b

--- lab_scripts/mains/baseline_official/main.py
+++
@@ -1,7 +0,0 @@
-from lab_scripts.models.baselines import official
-
-def predict(input_train_mod1, input_train_mod2, input_test_mod1):
-    return official.fit_predict(input_train_mod1, input_train_mod2, input_test_mod1)
-
-if __name__=='__main__':
-    predict()
