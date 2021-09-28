94748fbc4a6328ffb7070c9a498455e663e1823d
Simon Steshin <steshinsimon@gmail.com>
2021-09-28 19:37:22 +0300

    Add R environment
    Former-commit-id: 2bab6de994e249d9dd9ce836e7d0528e179060b7
    Former-commit-id: 5f89ff4ee84cff77d58e51dbb76df385b621160f

---
+++ r_packages.R
@@ -0,0 +1,13 @@
+# See instructions here: https://www.notion.so/R-work-in-progress-b3fd6b10d895483f979306fbff4900b0
+
+bioc_packages <- c(
+  "SingleCellExperiment",
+  "scran"
+)
+
+cran_packages <- c(
+  "BiocManager"
+)
+
+install.packages(cran_packages, Ncpus=8)
+BiocManager::install(bioc_packages, Ncpus=8)
