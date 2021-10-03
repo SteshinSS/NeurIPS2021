511c78795d9ad59323e9e2fe8c1a5ccce8a7a97c
Simon Steshin <steshinsimon@gmail.com>
2021-10-03 17:35:18 +0300

    Add download azimuth dataset
    Former-commit-id: fd0f4a108bde582b571a6b96cfdda1d329c7eee2
    Former-commit-id: 35aa6a575527a716d78797f2fdd41af102505e05

--- lab_scripts/data/download/azimuth.py
+++ lab_scripts/data/download/azimuth.py
@@ -1,83 +1,84 @@
-import urllib.request
-import tempfile
 import anndata as ad
-import scanpy as sc
 import pandas as pd
+import logging
 
 import tarfile
-import numpy as np
 import gzip
 from scipy import io
 
-par = {
-    "id": "azimuth_ref",
-    "input_count": "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE164378&format=file",
-    "input_meta": "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE164378&format=file&file=GSE164378%5Fsc%2Emeta%2Edata%5F3P%2Ecsv%2Egz",
-    "organism": "human",
-    "output_rna": "output_rna.h5ad",
-    "output_mod2": "output_mod2.h5ad"
-}
+import sys
+from pathlib import Path
 
-tar_temp = tempfile.NamedTemporaryFile()
-url = par['input_count']
-urllib.request.urlretrieve(url, tar_temp.name)
+sys.path.append(str(Path.cwd()))
 
-meta_temp = tempfile.NamedTemporaryFile()
-url = par['input_meta']
-urllib.request.urlretrieve(url, meta_temp.name)
+from lab_scripts.utils import utils
 
-samples = ['GSM5008737_RNA_3P', 'GSM5008738_ADT_3P'] # first sample is rna, second is protein data
-adatas = [] 
+URL_COUNTS = "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE164378&format=file"
+URL_META = "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE164378&format=file&file=GSE164378%5Fsc%2Emeta%2Edata%5F3P%2Ecsv%2Egz"
+PATH_GEX = "data/raw/gex_adt/azimuth_gex.h5ad"
+PATH_ADT = "data/raw/gex_adt/azimuth_adt.h5ad"
+UNS = {"dataset_id": "azimuth", "organism": "human"}
 
-with tarfile.open(tar_temp.name, 'r') as tar:
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger("azimuth_download")
+
+log.info("Downloading from %s...", URL_COUNTS)
+tar_temp = utils.download_to_tempfile(URL_COUNTS)
+samples = [
+    "GSM5008737_RNA_3P",
+    "GSM5008738_ADT_3P",
+]  # first sample is rna, second is protein data
+adatas = []
+
+log.info("Downloading is finished. Splitting...")
+
+with tarfile.open(tar_temp.name, "r") as tar:
     for sample in samples:
-        with gzip.open(tar.extractfile(sample + '-matrix.mtx.gz'), 'rb') as mm:
+        with gzip.open(tar.extractfile(sample + "-matrix.mtx.gz"), "rb") as mm:  # type: ignore
             X = io.mmread(mm).T.tocsr()
         obs = pd.read_csv(
-            tar.extractfile(sample + '-barcodes.tsv.gz'), 
-            compression='gzip',
-            header=None, 
-            sep='\t',
-            index_col=0
+            tar.extractfile(sample + "-barcodes.tsv.gz"),
+            compression="gzip",
+            header=None,
+            sep="\t",
+            index_col=0,
         )
         obs.index.name = None
         var = pd.read_csv(
-            tar.extractfile(sample + '-features.tsv.gz'), 
-            compression='gzip',
-            header=None, 
-            sep='\t'
+            tar.extractfile(sample + "-features.tsv.gz"),
+            compression="gzip",
+            header=None,
+            sep="\t",
         ).iloc[:, :1]
-        var.columns = ['names']
-        var.index = var['names'].values
+        var.columns = ["names"]
+        var.index = var["names"].values
         adata = ad.AnnData(X=X, obs=obs, var=var)
 
         adata.var_names_make_unique()
         adatas.append(adata)
     tar.close()
 
-adata_RNA = adatas[0]
-adata_ADT = adatas[1]
+meta_temp = utils.download_to_tempfile(URL_META)
+meta = pd.read_csv(meta_temp.name, index_col=0, compression="gzip")
 
+adata_gex = adatas[0]
+meta_gex = meta.loc[:, ~meta.columns.str.endswith("ADT")]
+adata_gex.obs = adata_gex.obs.join(meta_gex).rename(
+    columns={"Batch": "seq_batch", "donor": "batch"}
+)
+adata_gex.obs["cell_type"] = adata_gex.obs["celltype.l2"]
+adata_gex.var["feature_types"] = "GEX"
+adata_gex.uns = UNS
+adata_gex.write_h5ad(PATH_GEX, compression="gzip")
+log.info("GEX dataset in downloaded to %s", PATH_GEX)
 
-meta = pd.read_csv(meta_temp.name, index_col = 0, compression = "gzip")
-meta_adt = meta.loc[:,~meta.columns.str.endswith('RNA')]
-meta_rna = meta.loc[:,~meta.columns.str.endswith('ADT')]
-
-# set obs
-adata_RNA.obs = adata_RNA.obs.join(meta_rna).rename(columns = {'Batch':'seq_batch', 'donor':'batch'})
-adata_RNA.obs['cell_type'] = adata_RNA.obs['celltype.l2']
-adata_ADT.obs = adata_ADT.obs.join(meta_adt).rename(columns = {'Batch':'seq_batch', 'donor':'batch'})
-adata_ADT.obs['cell_type'] = adata_ADT.obs['celltype.l2']
-
-#  set var
-adata_RNA.var['feature_types'] = "GEX"
-adata_ADT.var['feature_types'] = "ADT"
-
-# set uns 
-uns = { "dataset_id" : par["id"], "organism" : par["organism"] }
-adata_RNA.uns = uns
-adata_ADT.uns = uns
-
-# save output
-adata_RNA.write_h5ad("data/raw/gex_adt/azimuth_RNA.h5ad", compression = "gzip")
-adata_ADT.write_h5ad(par['data/raw/gex_adt/azimuth_ADT.h5ad'], compression = "gzip")
+adata_adt = adatas[1]
+meta_adt = meta.loc[:, ~meta.columns.str.endswith("RNA")]
+adata_adt.obs = adata_adt.obs.join(meta_adt).rename(
+    columns={"Batch": "seq_batch", "donor": "batch"}
+)
+adata_adt.obs["cell_type"] = adata_adt.obs["celltype.l2"]
+adata_adt.var["feature_types"] = "ADT"
+adata_adt.uns = UNS
+adata_adt.write_h5ad(PATH_ADT, compression="gzip")
+log.info("ADT dataset in downloaded to %s", PATH_ADT)
