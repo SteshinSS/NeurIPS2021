76f6cec38fcfa0369a4e61469797cdca0905cac1
Simon Steshin <steshinsimon@gmail.com>
2021-10-05 14:37:23 +0300

    Rename dataset in dataloader
    Former-commit-id: 8f3e426b14d7a4c89fc8f9d80b4cf7bd1de6b207
    Former-commit-id: db99d6a84c056ca04bdca927e910ab468f0bc729

--- lab_scripts/data/dataloader.py
+++ lab_scripts/data/dataloader.py
@@ -9,25 +9,25 @@ them into this module, so you need only to call dataloader.load_data('my_dataset
 import anndata as ad
 
 library = {
-    "pm/official/adt_to_gex": {
+    "mp/official/adt_to_gex": {
         "train_mod1": "data/official/predict_modality/openproblems_bmmc_cite_phase1_mod2/openproblems_bmmc_cite_phase1_mod2.censor_dataset.output_train_mod1.h5ad",
         "train_mod2": "data/official/predict_modality/openproblems_bmmc_cite_phase1_mod2/openproblems_bmmc_cite_phase1_mod2.censor_dataset.output_train_mod2.h5ad",
         "test_mod1": "data/official/predict_modality/openproblems_bmmc_cite_phase1_mod2/openproblems_bmmc_cite_phase1_mod2.censor_dataset.output_test_mod1.h5ad",
         "test_mod2": "data/official/predict_modality/openproblems_bmmc_cite_phase1_mod2/openproblems_bmmc_cite_phase1_mod2.censor_dataset.output_test_mod2.h5ad",
     },
-    "pm/official/gex_to_adt": {
+    "mp/official/gex_to_adt": {
         "train_mod1": "data/official/predict_modality/openproblems_bmmc_cite_phase1_rna/openproblems_bmmc_cite_phase1_rna.censor_dataset.output_train_mod1.h5ad",
         "train_mod2": "data/official/predict_modality/openproblems_bmmc_cite_phase1_rna/openproblems_bmmc_cite_phase1_rna.censor_dataset.output_train_mod2.h5ad",
         "test_mod1": "data/official/predict_modality/openproblems_bmmc_cite_phase1_rna/openproblems_bmmc_cite_phase1_rna.censor_dataset.output_test_mod1.h5ad",
         "test_mod2": "data/official/predict_modality/openproblems_bmmc_cite_phase1_rna/openproblems_bmmc_cite_phase1_rna.censor_dataset.output_test_mod2.h5ad",
     },
-    "pm/official/atac_to_gex": {
+    "mp/official/atac_to_gex": {
         "train_mod1": "data/official/predict_modality/openproblems_bmmc_multiome_phase1_mod2/openproblems_bmmc_multiome_phase1_mod2.censor_dataset.output_train_mod1.h5ad",
         "train_mod2": "data/official/predict_modality/openproblems_bmmc_multiome_phase1_mod2/openproblems_bmmc_multiome_phase1_mod2.censor_dataset.output_train_mod2.h5ad",
         "test_mod1": "data/official/predict_modality/openproblems_bmmc_multiome_phase1_mod2/openproblems_bmmc_multiome_phase1_mod2.censor_dataset.output_test_mod1.h5ad",
         "test_mod2": "data/official/predict_modality/openproblems_bmmc_multiome_phase1_mod2/openproblems_bmmc_multiome_phase1_mod2.censor_dataset.output_test_mod2.h5ad",
     },
-    "pm/official/gex_to_atac": {
+    "mp/official/gex_to_atac": {
         "train_mod1": "data/official/predict_modality/openproblems_bmmc_multiome_phase1_rna/openproblems_bmmc_multiome_phase1_rna.censor_dataset.output_train_mod1.h5ad",
         "train_mod2": "data/official/predict_modality/openproblems_bmmc_multiome_phase1_rna/openproblems_bmmc_multiome_phase1_rna.censor_dataset.output_train_mod2.h5ad",
         "test_mod1": "data/official/predict_modality/openproblems_bmmc_multiome_phase1_rna/openproblems_bmmc_multiome_phase1_rna.censor_dataset.output_test_mod1.h5ad",
@@ -41,10 +41,10 @@ def load_data(name):
 
     Current datasets:
         Official:
-            'pm/official/gex_to_adt'
-            'pm/official/adt_to_gex'
-            'pm/official/atac_to_gex'
-            'pm/official/gex_to_atac'
+            'mp/official/gex_to_adt'
+            'mp/official/adt_to_gex'
+            'mp/official/atac_to_gex'
+            'mp/official/gex_to_atac'
     """
     files = library[name]
     result = dict()
