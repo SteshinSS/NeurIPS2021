Fix train metric calculation

lab_scripts/mains/x_autoencoder/main.py
@@ -6,16 +6,13 @@ import pytorch_lightning as pl
 import torch
 import yaml  # type: ignore
 from lab_scripts.data import dataloader
-from lab_scripts.mains.x_autoencoder import preprocessing
+from lab_scripts.mains.x_autoencoder import preprocessing, tune
 from lab_scripts.mains.x_autoencoder.preprocessing import (
-    base_checkpoint_path,
-    base_config_path,
-)
-from lab_scripts.mains.x_autoencoder import tune
+    base_checkpoint_path, base_config_path)
 from lab_scripts.models import x_autoencoder
 from lab_scripts.utils import utils
-from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import LearningRateMonitor
+from pytorch_lightning.loggers import WandbLogger
 from scipy.sparse import csr_matrix
 
 log = logging.getLogger("x_autoencoder")
@@ -172,11 +169,12 @@ def train(config: dict):
     )
     train_dataloader = preprocessed_data["train_dataloader"]
     test_dataloader = preprocessed_data["test_dataloader"]
-    small_train_dataloader = preprocessed_data['small_train_dataloader']
+    small_train_dataloader = preprocessed_data["small_dataloader"]
+    small_idx = preprocessed_data["small_idx"]
     first_test_inverse = preprocessed_data["first_test_inverse"]
     second_test_inverse = preprocessed_data["second_test_inverse"]
-    first_small_inverse = preprocessed_data['first_small_inverse']
-    second_small_inverse = preprocessed_data['second_small_inverse']
+    first_train_inverse = preprocessed_data["first_train_inverse"]
+    second_train_inverse = preprocessed_data["second_train_inverse"]
 
     # Add input layer sizes
     model_config["first"]["input_features"] = preprocessed_data["first_input_features"]
@@ -189,7 +187,9 @@ def train(config: dict):
     model_config["second"]["target_features"] = preprocessed_data[
         "second_target_features"
     ]
-    model_config['total_batches'] = torch.unique(preprocessed_data['train_batch_idx']).shape[0]
+    model_config["total_batches"] = torch.unique(
+        preprocessed_data["train_batch_idx"]
+    ).shape[0]
     log.info("Data is preprocessed")
 
     # Configure training
@@ -203,14 +203,8 @@ def train(config: dict):
             config_exclude_keys=["wandb"],
         )
 
-        pl_logger.experiment.define_metric(
-            name="true_1_to_2",
-            summary="min"
-        )
-        pl_logger.experiment.define_metric(
-            name="true_2_to_1",
-            summary="min"
-        )
+        pl_logger.experiment.define_metric(name="true_1_to_2", summary="min")
+        pl_logger.experiment.define_metric(name="true_2_to_1", summary="min")
 
     validation_callback = x_autoencoder.TargetCallback(
         test_dataloader=test_dataloader,
@@ -221,16 +215,16 @@ def train(config: dict):
     )
     validation_train_callback = x_autoencoder.TargetCallback(
         test_dataloader=small_train_dataloader,
-        first_inverse=first_small_inverse,
-        first_true_target=dataset["train_mod1"][:512],
-        second_inverse=second_small_inverse,
-        second_true_target=dataset["train_mod2"][:512],
-        prefix='train',
+        first_inverse=first_train_inverse,
+        first_true_target=dataset["train_mod1"][small_idx],
+        second_inverse=second_train_inverse,
+        second_true_target=dataset["train_mod2"][small_idx],
+        prefix="train",
     )
     callbacks = [validation_callback, validation_train_callback]
 
     learning_rate_monitor = LearningRateMonitor(
-        logging_interval='step',
+        logging_interval="step",
     )
     if pl_logger:
         callbacks.append(learning_rate_monitor)

lab_scripts/mains/x_autoencoder/preprocessing.py
@@ -2,12 +2,10 @@ import logging
 import pickle
 
 import anndata as ad
+import numpy as np
 import torch
-from lab_scripts.data.integration.processor import (
-    FourOmicsDataset,
-    Processor,
-    TwoOmicsDataset,
-)
+from lab_scripts.data.integration.processor import (FourOmicsDataset,
+                                                    Processor, TwoOmicsDataset)
 from torch.utils.data import DataLoader
 
 log = logging.getLogger("x_autoencoder")
@@ -31,10 +29,10 @@ def get_processor_path(
 
 def load_processor(config, task_type, mod, proc_type=None):
     processor_checkpoint_path = get_processor_path(config, task_type, mod, proc_type)
-    with open(processor_checkpoint_path, 'rb') as f:
+    with open(processor_checkpoint_path, "rb") as f:
         processor = pickle.load(f)
     if not proc_type:
-        proc_type = ''
+        proc_type = ""
     log.info(f"{mod} {proc_type} processor is loaded from {processor_checkpoint_path}")
     return processor
 
@@ -77,7 +75,15 @@ def get_batch_idx(dataset: ad.AnnData):
     return batch_idx
 
 
-def apply_processor(config: dict, dataset_train: ad.AnnData, dataset_test: ad.AnnData, mod: str, task_type: str, is_train: bool, proc_type: str = None):
+def apply_processor(
+    config: dict,
+    dataset_train: ad.AnnData,
+    dataset_test: ad.AnnData,
+    mod: str,
+    task_type: str,
+    is_train: bool,
+    proc_type: str = None,
+):
     if is_train:
         processor = Processor(config, mod)
         train_X, train_inverse = processor.fit_transform(dataset_train)
@@ -91,39 +97,7 @@ def apply_processor(config: dict, dataset_train: ad.AnnData, dataset_test: ad.An
         return train_X, train_inverse, test_X, test_inverse
 
 
-
-def preprocess_mirror_dataset(
-    mod_config: dict,
-    dataset_train: ad.AnnData,
-    dataset_test: ad.AnnData,
-    task_type: str,
-    is_train: bool,
-):
-    """Processes a mirror dataset
-
-    Args:
-        mod_config (dict): Modality data configuration
-        dataset_train (ad.AnnData): Train dataset
-        dataset_test (ad.AnnData): Test dataset
-        task_type (str): One of four
-
-    Returns:
-        tuple: train dict, test dict and number of features
-    """
-    mod = mod_config['name']
-    train_X, train_inverse, test_X, test_inverse = apply_processor(mod_config, dataset_train, dataset_test, mod, task_type, is_train)
-
-    train_batch_idx = get_batch_idx(dataset_train)
-    test_batch_idx = get_batch_idx(dataset_test)
-
-    train = {"X": train_X, "inverse": train_inverse, "batch_idx": train_batch_idx}
-    test = {"X": test_X, "inverse": test_inverse, "batch_idx": test_batch_idx}
-    n_features = train_X.shape[1]
-
-    return train, test, n_features, n_features
-
-
-def preprocess_not_mirror_dataset(
+def preprocess_one_dataset(
     mod_config: dict,
     dataset_train: ad.AnnData,
     dataset_test: ad.AnnData,
@@ -141,9 +115,25 @@ def preprocess_not_mirror_dataset(
     Returns:
         tuple: train dict, test dict and number of features
     """
-    mod = mod_config['name']
-    input_train_X, _, input_test_X, _ = apply_processor(mod_config["inputs"], dataset_train, dataset_test, mod, task_type, is_train, "input")
-    target_train_X, train_inverse, target_test_X, test_inverse = apply_processor(mod_config["targets"], dataset_train, dataset_test, mod, task_type, is_train, "targets")
+    mod = mod_config["name"]
+    input_train_X, _, input_test_X, _ = apply_processor(
+        mod_config["inputs"],
+        dataset_train,
+        dataset_test,
+        mod,
+        task_type,
+        is_train,
+        "input",
+    )
+    target_train_X, train_inverse, target_test_X, test_inverse = apply_processor(
+        mod_config["targets"],
+        dataset_train,
+        dataset_test,
+        mod,
+        task_type,
+        is_train,
+        "targets",
+    )
 
     train_batch_idx = get_batch_idx(dataset_train)
     test_batch_idx = get_batch_idx(dataset_test)
@@ -168,35 +158,7 @@ def preprocess_not_mirror_dataset(
     return train, test, inputs_features, target_features
 
 
-def preprocess_one_dataset(
-    mod_config: dict,
-    dataset_train: ad.AnnData,
-    dataset_test: ad.AnnData,
-    task_type: str,
-    is_train: bool,
-):
-    """Processes dataset for one modality
-
-    Args:
-        mod_config (dict): Modality data configuration
-        dataset_train (ad.AnnData): Train dataset
-        dataset_test (ad.AnnData): Test dataset
-        task_type (str): One of four tasks
-
-    Returns:
-        tuple: train dict, test dict and number of features
-    """
-    if "inputs" in mod_config:
-        return preprocess_not_mirror_dataset(
-            mod_config, dataset_train, dataset_test, task_type, is_train
-        )
-    else:
-        return preprocess_mirror_dataset(
-            mod_config, dataset_train, dataset_test, task_type, is_train
-        )
-
-
-def get_dataset_arguments(first, is_first_mirror, second, is_second_mirror):
+def get_dataset_arguments(first, second):
     """Returns list of arguments for FourOmicsDataset construction.
 
     Args:
@@ -209,19 +171,10 @@ def get_dataset_arguments(first, is_first_mirror, second, is_second_mirror):
         List: Of arguments
     """
     arguments = []
-    if is_first_mirror:
-        arguments.append(first["X"])
-        arguments.append(first["X"])
-    else:
-        arguments.append(first["input_X"])
-        arguments.append(first["target_X"])
-
-    if is_second_mirror:
-        arguments.append(second["X"])
-        arguments.append(second["X"])
-    else:
-        arguments.append(second["input_X"])
-        arguments.append(second["target_X"])
+    arguments.append(first["input_X"])
+    arguments.append(first["target_X"])
+    arguments.append(second["input_X"])
+    arguments.append(second["target_X"])
     arguments.append(first["batch_idx"])
     return arguments
 
@@ -238,83 +191,132 @@ def preprocess_data(config: dict, dataset, batch_size, is_train):
         dict: returns train/test dataloaders, functions to invert preprocessing
         transformation and number of features in each modalities.
     """
-    first_train, first_test, first_input_features, first_target_features = preprocess_one_dataset(
-        config["mod1"], dataset["train_mod1"], dataset["test_mod1"], config["task_type"], is_train
+    (
+        first_train,
+        first_test,
+        first_input_features,
+        first_target_features,
+    ) = preprocess_one_dataset(
+        config["mod1"],
+        dataset["train_mod1"],
+        dataset["test_mod1"],
+        config["task_type"],
+        is_train,
     )
-    second_train, second_test, second_input_features, second_target_features = preprocess_one_dataset(
-        config["mod2"], dataset["train_mod2"], dataset["test_mod2"], config["task_type"], is_train
-    )
-    first_small, _, _, _ = preprocess_one_dataset(
-        config['mod1'], dataset['train_mod1'][:512], dataset['test_mod1'][:50], config['task_type'], is_train
-    )
-    second_small, _, _, _ =preprocess_one_dataset(
-        config['mod2'], dataset['train_mod2'][:512], dataset['test_mod2'][:50], config['task_type'], is_train
+    (
+        second_train,
+        second_test,
+        second_input_features,
+        second_target_features,
+    ) = preprocess_one_dataset(
+        config["mod2"],
+        dataset["train_mod2"],
+        dataset["test_mod2"],
+        config["task_type"],
+        is_train,
     )
 
     if torch.cuda.is_available():
         cuda = True
     else:
         cuda = False
-    is_first_mirror = "inputs" not in config["mod1"]
-    is_second_mirror = "inputs" not in config["mod2"]
-    if is_first_mirror and is_second_mirror:
-        train_dataset = TwoOmicsDataset(
-            first_train["X"], second_train["X"], first_train["batch_idx"]
-        )
-        test_dataset = TwoOmicsDataset(
-            first_test["X"], second_test["X"], first_test["batch_idx"]
-        )
-        small_dataset = TwoOmicsDataset(
-            first_small['X'], second_small['X'], first_small['batch_idx']
-        )
-    else:
-        train_arguments = get_dataset_arguments(
-            first_train, is_first_mirror, second_train, is_second_mirror
-        )
-        train_dataset = FourOmicsDataset(*train_arguments)
-        test_arguments = get_dataset_arguments(
-            first_test, is_first_mirror, second_test, is_second_mirror
-        )
-        test_dataset = FourOmicsDataset(*test_arguments)
-        small_arguments = get_dataset_arguments(
-            first_small, is_first_mirror, second_small, is_second_mirror
-        )
-        small_dataset = FourOmicsDataset(*small_arguments)
-    
-    if cuda:
-        test_dataset.to('cuda')
-        small_dataset.to('cuda')
-    
+
+    train_arguments = get_dataset_arguments(first_train, second_train)
+    train_dataset = FourOmicsDataset(*train_arguments)
     train_dataloader = DataLoader(
         train_dataset,
         batch_size=batch_size,
-        shuffle=True,
+        shuffle=is_train,
         pin_memory=cuda,
         num_workers=1,
     )
-    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    small_idx = np.arange(len(train_dataset))
+    np.random.shuffle(small_idx)
+    small_idx = small_idx[:512]
     small_train_dataloader = DataLoader(
-        small_dataset,
+        torch.utils.data.Subset(train_dataset, small_idx),
         batch_size=batch_size,
         shuffle=False,
     )
+
+    test_arguments = get_dataset_arguments(first_test, second_test)
+    test_dataset = FourOmicsDataset(*test_arguments)
+    if cuda:
+        test_dataset.to("cuda")
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
     result = {
         "train_dataloader": train_dataloader,
         "test_dataloader": test_dataloader,
-        "small_train_dataloader": small_train_dataloader,
-
+        "small_dataloader": small_train_dataloader,
+        "small_idx": small_idx,
         "first_train_inverse": first_train["inverse"],
         "first_test_inverse": first_test["inverse"],
-        "first_small_inverse": first_small['inverse'],
         "first_input_features": first_input_features,
         "first_target_features": first_target_features,
-        
         "second_train_inverse": second_train["inverse"],
         "second_test_inverse": second_test["inverse"],
-        'second_small_inverse': second_small['inverse'],
         "second_input_features": second_input_features,
         "second_target_features": second_target_features,
-
-        'train_batch_idx': first_train['batch_idx'],
+        "train_batch_idx": first_train["batch_idx"],
     }
     return result
+
+
+def _test_dataset(dataloader, first_transform, second_transform, dataset):
+    first = []
+    second = []
+    for batch in dataloader:
+        inputs, targets, batch_idx = batch
+        first.append(targets[0])
+        second.append(targets[1])
+    first = torch.cat(first, dim=0).cpu()
+    first = first_transform(first)
+    first_true = dataset["test_mod1"].X.toarray()
+    difference = first - first_true
+    print(difference.sum())
+
+    second = torch.cat(second, dim=0).cpu()
+    second = second_transform(second)
+    second_true = dataset["test_mod2"].X.toarray()
+    difference = second - second_true
+    print(difference.sum())
+
+
+def _test_1():
+    dataset = dataloader.load_data("mp/official/gex_to_adt")
+    config = {
+        "task_type": "gex_to_adt",
+        "mod1": {
+            "name": "gex",
+            "inputs": {
+                "use_normalized": True,
+                "scale": True,
+                "type": "float",
+            },
+            "targets": {"use_normalized": True, "scale": False, "type": "float"},
+        },
+        "mod2": {
+            "name": "adt",
+            "inputs": {
+                "use_normalized": True,
+                "scale": True,
+                "type": "float",
+            },
+            "targets": {"use_normalized": True, "scale": False, "type": "float"},
+        },
+    }
+    preprocessed_data = preprocess_data(config, dataset, 128, True)
+    _test_dataset(
+        preprocessed_data["test_dataloader"],
+        preprocessed_data["first_test_inverse"],
+        preprocessed_data["second_test_inverse"],
+        dataset,
+    )
+
+
+if __name__ == "__main__":
+    from lab_scripts.data import dataloader
+
+    _test_1()

lab_scripts/models/x_autoencoder/model.py
@@ -1,20 +1,19 @@
 import logging
-from typing import Callable, List
 from collections import OrderedDict
+from typing import Callable, List
 
 import anndata as ad
-from numpy import diff
+import numpy as np
 import pytorch_lightning as pl
 import torch
-from torch.nn import functional as F
+import wandb
 from lab_scripts.metrics import mp
 from lab_scripts.utils import utils
-import numpy as np
-from torch import nn
-from torch.distributions.negative_binomial import NegativeBinomial
 from pyro.distributions.zero_inflated import ZeroInflatedNegativeBinomial
+from torch import nn
 from torch.distributions.log_normal import LogNormal
-import wandb
+from torch.distributions.negative_binomial import NegativeBinomial
+from torch.nn import functional as F
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("x_autoencoder")
@@ -58,28 +57,30 @@ def get_activation(activation_name: str):
         return nn.LeakyReLU()
     elif activation_name == "tanh":
         return nn.Tanh()
-    elif activation_name == 'selu':
+    elif activation_name == "selu":
         return nn.SELU()
-    elif activation_name == 'relu':
+    elif activation_name == "relu":
         return nn.ReLU()
 
 
 def selu_init(layer):
     if not isinstance(layer, nn.Linear):
         return
-    torch.nn.init.kaiming_normal_(layer.weight, mode='fan_in', nonlinearity='linear')
+    torch.nn.init.kaiming_normal_(layer.weight, mode="fan_in", nonlinearity="linear")
     torch.nn.init.constant_(layer.bias, 0)
+
 
 def relu_init(layer):
     if not isinstance(layer, nn.Linear):
         return
     torch.nn.init.orthogonal_(layer.weight)
-    torch.nn.init.constant_(layer.bias, 0)   
+    torch.nn.init.constant_(layer.bias, 0)
+
 
 def init(net, activation):
-    if activation == 'selu':
+    if activation == "selu":
         net.apply(selu_init)
-    elif activation == 'relu':
+    elif activation == "relu":
         net.apply(relu_init)
 
 
@@ -93,12 +94,12 @@ class Encoder(pl.LightningModule):
 
         net = []
         for i in range(len(dims) - 1):
-            net.append((f'{i}_Linear', nn.Linear(dims[i], dims[i + 1])))
-            net.append((f'{i}_Activation', activation))  # type: ignore
-            if i - 1 in config['encoder_bn']:
-                net.append((f'{i}_BatchNorm', nn.BatchNorm1d(dims[i+1])))  # type: ignore
+            net.append((f"{i}_Linear", nn.Linear(dims[i], dims[i + 1])))
+            net.append((f"{i}_Activation", activation))  # type: ignore
+            if i - 1 in config["encoder_bn"]:
+                net.append((f"{i}_BatchNorm", nn.BatchNorm1d(dims[i + 1])))  # type: ignore
         self.net = nn.Sequential(OrderedDict(net))
-        init(self.net, config['activation'])
+        init(self.net, config["activation"])
 
     def forward(self, x):
         return self.net(x)
@@ -154,17 +155,17 @@ class MSEDecoder(pl.LightningModule):
         dims.insert(0, latent_dim)
         dims.append(config["target_features"])
         activation = get_activation(config["activation"])
-        batchnorm_pos = config['decoder_bn']
+        batchnorm_pos = config["decoder_bn"]
 
         net = []
         for i in range(len(dims) - 1):
-            net.append((f'{i}_Linear', nn.Linear(dims[i], dims[i + 1])))
+            net.append((f"{i}_Linear", nn.Linear(dims[i], dims[i + 1])))
             if i + 1 != len(dims) - 1:
-                net.append((f'{i}_Activation', activation))
+                net.append((f"{i}_Activation", activation))
             if i - 1 in batchnorm_pos:
-                net.append((f'{i}_BatchNorm', nn.BatchNorm1d(dims[i+1])))  # type: ignore
+                net.append((f"{i}_BatchNorm", nn.BatchNorm1d(dims[i + 1])))  # type: ignore
         self.net = nn.Sequential(OrderedDict(net))
-        init(self.net, config['activation'])
+        init(self.net, config["activation"])
 
     def forward(self, x):
         return self.net(x)
@@ -345,9 +346,9 @@ class X_autoencoder(pl.LightningModule):
     def __init__(self, config: dict):
         super().__init__()
         self.lr = config["lr"]
-        self.attack = config['attack']
-        self.sustain = config['sustain']
-        self.release = config['release']
+        self.attack = config["attack"]
+        self.sustain = config["sustain"]
+        self.release = config["release"]
         self.patience = config["patience"]
         self.vae = config["vae"]
         self.use_mmd = config["use_mmd_loss"]
@@ -358,8 +359,8 @@ class X_autoencoder(pl.LightningModule):
         self.critic_lambda = config["critic_lambda"]
         self.critic_type = config["critic_type"]
         self.critic_lr = config["critic_lr"]
-        self.critic_iterations = config['critic_iterations']
-        self.gradient_clip = config['gradient_clip']
+        self.critic_iterations = config["critic_iterations"]
+        self.gradient_clip = config["gradient_clip"]
 
         first_config = config["first"]
         self.first_config = first_config
@@ -413,8 +414,8 @@ class X_autoencoder(pl.LightningModule):
         }
 
         if self.use_critic:
-            result['first_critic'] = self.first_critic(first_latent)
-            result['second_critic'] = self.second_critic(second_latent)
+            result["first_critic"] = self.first_critic(first_latent)
+            result["second_critic"] = self.second_critic(second_latent)
 
         return result
 
@@ -425,10 +426,6 @@ class X_autoencoder(pl.LightningModule):
         second_latent = self.second_encoder(second)
         second_critic = self.second_critic(second_latent.detach())
         return first_critic, second_critic
-    
-    def training_epoch_end(self, outputs):
-        sch = self.lr_schedulers()
-        sch.step()
 
     def training_step(self, batch, batch_n):
         if len(batch) == 2:
@@ -439,14 +436,18 @@ class X_autoencoder(pl.LightningModule):
         first, second = inputs
 
         optimizers = self.optimizers()
-        
+
         if self.use_critic:
             first_critic, second_critic = self.critic_forward(first, second)
             critic_optimizer = optimizers[1]
             critic_optimizer.zero_grad()
-            critic_loss = self.calculate_critic_loss(first_critic, second_critic, batch_idx)
+            critic_loss = self.calculate_critic_loss(
+                first_critic, second_critic, batch_idx
+            )
             self.manual_backward(critic_loss)
-            torch.nn.utils.clip_grad_norm_(self.critic_parameters, self.gradient_clip, error_if_nonfinite=True)
+            torch.nn.utils.clip_grad_norm_(
+                self.critic_parameters, self.gradient_clip, error_if_nonfinite=True
+            )
             critic_optimizer.step()
             main_optimizer = optimizers[0]
 
@@ -459,10 +460,18 @@ class X_autoencoder(pl.LightningModule):
         main_optimizer.zero_grad()
         loss = self.calculate_loss(result, targets, batch_idx, batch_n)
         self.manual_backward(loss)
-        torch.nn.utils.clip_grad_norm_(self.main_parameters, self.gradient_clip, error_if_nonfinite=True)
+        torch.nn.utils.clip_grad_norm_(
+            self.main_parameters, self.gradient_clip, error_if_nonfinite=True
+        )
         main_optimizer.step()
-        self.log("train_loss", loss, on_step=True, prog_bar=True, logger=True, on_epoch=False)
+        self.log(
+            "train_loss", loss, on_step=True, prog_bar=True, logger=True, on_epoch=False
+        )
         return
+
+    def training_epoch_end(self, outputs):
+        sch = self.lr_schedulers()
+        sch.step()
 
     def calculate_loss(self, result: dict, targets, batch_idx, batch_n):
         first_target, second_target = targets
@@ -506,6 +515,7 @@ class X_autoencoder(pl.LightningModule):
             prog_bar=True,
             logger=True,
         )
+
         loss = first_to_first + first_to_second + second_to_first + second_to_second
         if self.use_mmd:
             mmd_loss = calculate_mmd_loss(result["first_latent"], batch_idx)
@@ -541,16 +551,18 @@ class X_autoencoder(pl.LightningModule):
                 logger=True,
             )
             loss += sim_loss  # type: ignore
-        
+
         if self.use_critic:
-            critic_loss = self.first_critic.get_loss(result["first_critic"], batch_idx, shifted=True)
+            critic_loss = self.first_critic.get_loss(
+                result["first_critic"], batch_idx, shifted=True
+            )
             critic_loss += self.second_critic.get_loss(
                 result["second_critic"], batch_idx, shifted=True
             )
             critic_loss *= self.critic_lambda
-            #if self.current_epoch < 5:
+            # if self.current_epoch < 5:
             #    critic_loss = 0.0
-            #else:
+            # else:
             #    critic_loss = min(1.0, (1.0/10.0)* (self.current_epoch - 5)) * critic_loss
             self.log(
                 "train_critic",
@@ -562,12 +574,10 @@ class X_autoencoder(pl.LightningModule):
             )
             loss += critic_loss
         return loss
-    
+
     def calculate_critic_loss(self, first_critic, second_critic, batch_idx):
         loss = self.first_critic.get_loss(first_critic, batch_idx)
-        loss += self.second_critic.get_loss(
-            second_critic, batch_idx
-        )
+        loss += self.second_critic.get_loss(second_critic, batch_idx)
         self.log(
             "critic",
             loss,
@@ -584,6 +594,8 @@ class X_autoencoder(pl.LightningModule):
         elif len(batch) == 3:
             inputs, _, _ = batch
         first, second = inputs
+        first = first.to(self.device)
+        second = second.to(self.device)
         result = self(first, second)
         first_to_second = result["first_to_second"]
         second_to_first = result["second_to_first"]
@@ -610,21 +622,15 @@ class X_autoencoder(pl.LightningModule):
             return lr_scale
 
         lr_scheduler = torch.optim.lr_scheduler.LambdaLR(
-            main_optimizer,
-            lr_lambda=lr_foo
+            main_optimizer, lr_lambda=lr_foo
         )
-        optimizers = [
-            {
-                "optimizer": main_optimizer,
-                "lr_scheduler": lr_scheduler
-            }
-        ]
+        optimizers = [{"optimizer": main_optimizer, "lr_scheduler": lr_scheduler}]
         if self.use_critic:
             critic_optimizer = torch.optim.Adam(
                 self.critic_parameters,
                 lr=self.critic_lr,
             )
-            optimizers.append({'optimizer': critic_optimizer})
+            optimizers.append({"optimizer": critic_optimizer})
         return tuple(optimizers)
 
 
@@ -655,7 +661,7 @@ class TargetCallback(pl.Callback):
         first_true_target=None,
         second_inverse=None,
         second_true_target=None,
-        prefix=None
+        prefix=None,
     ):
         self.test_dataloader = test_dataloader
         self.first_inverse = first_inverse
@@ -663,7 +669,7 @@ class TargetCallback(pl.Callback):
         self.second_inverse = second_inverse
         self.second_true_target = second_true_target
         if not prefix:
-            prefix = 'true'
+            prefix = "true"
         self.prefix = prefix
 
     def on_train_epoch_end(self, trainer, pl_module):
@@ -673,12 +679,12 @@ class TargetCallback(pl.Callback):
             first_to_second_batch, second_to_first_batch = pl_module.predict_step(
                 batch, i
             )
-            first_to_second.append(first_to_second_batch)
-            second_to_first.append(second_to_first_batch)
+            first_to_second.append(first_to_second_batch.cpu().detach())
+            second_to_first.append(second_to_first_batch.cpu().detach())
 
         logger = trainer.logger
         if self.second_inverse is not None:
-            predictions = torch.cat(first_to_second, dim=0).cpu().detach()
+            predictions = torch.cat(first_to_second, dim=0)
             predictions = self.second_inverse(predictions)
             metric = mp.calculate_target(predictions, self.second_true_target)
             pl_module.log(self.prefix + "_1_to_2", metric, prog_bar=True)
@@ -690,14 +696,17 @@ class TargetCallback(pl.Callback):
                 )
 
         if self.first_inverse is not None:
-            predictions = torch.cat(second_to_first, dim=0).cpu().detach()
+            predictions = torch.cat(second_to_first, dim=0)
             predictions = self.first_inverse(predictions)
             metric = mp.calculate_target(predictions, self.first_true_target)
+
             pl_module.log(self.prefix + "_2_to_1", metric, prog_bar=True)
 
             difference = predictions - self.first_true_target.X.toarray()
             if logger:
-                logger.experiment.log({self.prefix + "_first_difference": wandb.Histogram(difference)})
+                logger.experiment.log(
+                    {self.prefix + "_first_difference": wandb.Histogram(difference)}
+                )
 
 
 def calculate_mmd_loss(X, batch_idx):